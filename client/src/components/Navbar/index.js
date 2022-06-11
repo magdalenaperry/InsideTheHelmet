cedd2d4bc1f05ab2c26ccdf207d6661ecd2661d7
Magdalena Perry <magperry16@gmail.com>
2022-06-11 12:31:23 -0400

    update logo size nav

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -22,13 +22,13 @@ function Navbar() {
             {/* <!-- Navbar brand --> */}
 
             <Link className="navbar-brand logo" to="/">
-              <img src={Logo} alt="logo" height="40px" />
+              <img className="ms-2" src={Logo} alt="logo" height="70px" />
 
               <span className="navBrand fs-5-lg ms-3">inside the helmet</span>
             </Link>
 
             {/* <!-- Toggle button --> */}
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+            <button className="navbar-toggler me-2" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
           aria-expanded="false" 
           aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
