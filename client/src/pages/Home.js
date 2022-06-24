0d09b5aadd0863daf0c90e887d7b8a9a24d6754d
Magdalena Perry <magperry16@gmail.com>
2022-06-24 10:54:36 -0400

    styling created for audio rendition

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import React from 'react';
 
 import { Link } from "react-router-dom";
 
+import Logo from "../assets/logo192.png";
+
 // import ReactPlayer from "react-player";
 // import PodcastPlayer from "@mrpollard/react-rss-podcast-player";
 // import ReactAudioPlayer from "react-audio-player";
@@ -40,6 +42,7 @@ const Home = () => {
 
   return (
     <>
+      {/* sign up button */}
       <main className="">
         <div className="container">
           <div className='mt-5'>
@@ -50,38 +53,115 @@ const Home = () => {
               </button>
             </Link>
           </div>
+        </div>
 
 
-          <br />
 
 
-          <div className="text-center border">
-            <h1>Testing Audio Player</h1>
-            <img src="https://via.placeholder.com/900x300" alt="" className='img-fluid' />
-            <AudioPlayer
-              // style={{ width: "300px" }}
-              style={{ borderRadius: 0 }}
-              // autoPlay
-              // layout="horizontal"
-              src={url}
-              onPlay={(e) => console.log("onPlay")}
-              showSkipControls={true}
-              showJumpControls={false}
-            // header={`Now playing: ${musicTracks[trackIndex].name}`}
-            // footer="All music from: www.bensound.com"
-            // onClickPrevious={handleClickPrevious}
-            // onClickNext={handleClickNext}
-            // onEnded={handleClickNext}
-            // other props here
-            />
+        <div className='container border'>
+          
+
+        </div>
+
+
+        <div className="container accordion" id="accordionExample">
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingOne">
+              <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Epsiode Name 1
+
+
+              </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                <div className='row'>
+
+                  <div className='col-md-2 text-md-center'>
+                    <img src={Logo} alt="" className='img-fluid' />
+                    <p>Episode Title Name</p>
+
+
+
+                  </div>
+                  <div className='col-md-7 text-md-center'>
+                    <div className=" border">
+                      <AudioPlayer
+                        // style={{ width: "300px" }}
+                        style={{ borderRadius: 0 }, {height: 150}}
+                        // autoPlay
+                        // layout="horizontal"
+                        src={url}
+                        onPlay={(e) => console.log("onPlay")}
+                        showSkipControls={true}
+                        showJumpControls={false}
+                      // header={`Now playing: ${musicTracks[trackIndex].name}`}
+                      // footer="All music from: www.bensound.com"
+                      // onClickPrevious={handleClickPrevious}
+                      // onClickNext={handleClickNext}
+                      // onEnded={handleClickNext}
+                      // other props here
+                      />
+                    </div>
+                  </div>
+                  <div className='col-md-3'>
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam expedita consequatur nisi tenetur ad fuga sint, similique reiciendis labore dolore aliquam.
+                    </p>
+                  </div>
+
+
+                </div>
+              </div>
+            </div>
           </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingTwo">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                Epsiode Name 2
+              </button>
+            </h2>
+            <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel odit, quos mollitia cupiditate esse reprehenderit sit consequatur, pariatur saepe ex nulla voluptas quis ipsum similique accusamus libero reiciendis, aut ipsam!
+              </div>
+            </div>
+          </div>
+          <div className="accordion-item">
+            <h2 className="accordion-header" id="headingThree">
+              <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                Episode Name 3
+              </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+              <div className="accordion-body">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum amet ut incidunt maxime nesciunt magnam ad explicabo quasi et? Esse aliquam ut, expedita officiis quia non illo explicabo praesentium harum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam ab minima officia excepturi enim esse nihil laborum explicabo. Corrupti quaerat quibusdam facilis vel repudiandae cum, consectetur maiores tempora beatae aliquam.
+              </div>
+            </div>
+          </div>
+        </div>
 
-<br />
-<br />
 
 
-          {/* with video formatting */}
-          {/* <p>- if we opt to include video feed as well, use reactplayer</p>
+
+
+      </main>
+
+
+
+    </>
+  );
+};
+
+export default Home;
+
+
+{/* <br />
+<br /> */}
+
+
+{/* with video formatting */ }
+{/* <p>- if we opt to include video feed as well, use reactplayer</p>
           <ReactPlayer
             controls
             className="react-player"
@@ -94,11 +174,11 @@ const Home = () => {
 
 
 
-          <br />
+{/* <br /> */ }
 
 
-          {/* RSS feed needed for this to work */}
-          {/* <p>-needs proper rss feed for this to work, use podcastplayer</p>
+{/* RSS feed needed for this to work */ }
+{/* <p>-needs proper rss feed for this to work, use podcastplayer</p>
           <PodcastPlayer url="https://anchor.fm/s/a1ec84ec/podcast/rss" />
           <br /> */}
 
@@ -106,11 +186,11 @@ const Home = () => {
 
 
 
-          <br />
+{/* <br /> */ }
 
 
-          {/* small audio file */}
-          {/* <div className='col-12'>
+{/* small audio file */ }
+{/* <div className='col-12'>
 <p>- small media player can include with titles, etc. Use ReactAudioPlayer</p>
             <img src="https://via.placeholder.com/900x300" alt="" className='img-fluid rounded border'/>
             <ReactAudioPlayer
@@ -132,19 +212,19 @@ const Home = () => {
 
 
 
-          {/* plays automatically*/}
-          {/* <ReactHowler src={url} playing={true} controls /> */}
-          <br />
+{/* plays automatically*/ }
+{/* <ReactHowler src={url} playing={true} controls /> */ }
+// <br />
 
 
 
 
-          <br />
+// <br />
 
 
 
-          {/* buttons: play and mute */}
-          {/* <Media>
+{/* buttons: play and mute */ }
+{/* <Media>
             <div className="media">
               <div className="media-player">
                 <Player src={url} />
@@ -158,7 +238,7 @@ const Home = () => {
 
 
 
-<p>- iFrame setup links to podcast, embedded into site.</p>
+{/* <p>- iFrame setup links to podcast, embedded into site.</p>
           <iframe src="https://anchor.fm/magdalena-perry/embed/episodes/Hey-Brother--Avicci-e1kbdn3" height="102px" width="400px" frameBorder="0" scrolling="no"></iframe>
 
 
@@ -174,14 +254,4 @@ const Home = () => {
 
           <br />
 
-          <br />
-        </div>
-      </main>
-
-
-
-    </>
-  );
-};
-
-export default Home;
+          <br /> */}
